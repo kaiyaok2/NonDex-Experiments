1c50b2c5ae9e710ef6ca3b0dfe8069a5b602b5ff
Kaiyao Ke <kaiyaok2@illinois.edu>
2024-01-25 03:26:53 +0000

    add maven scripts

---
+++ experiment_on_maven/find-recent-popular-repos.py
@@ -0,0 +1,25 @@
+# use github API to get a list of Gradle repos
+
+import requests
+import math
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 8:
+        print("You need to provide your Github username, your token, min_stars, max_stars, earliest_created, latest_created and pages")
+        sys.exit(1)
+    username, token, min_stars, max_stars, earliest_created, latest_created, pages = sys.argv[1:]
+    session = requests.Session()
+    session.auth = (username,token)
+
+    filter = "stars:" + min_stars + ".." + max_stars + "+" + "created:" + earliest_created + ".." + latest_created
+
+    f = open("repos.txt", "x")
+
+    for page in range(int(pages)):
+        r = session.get("https://api.github.com/search/repositories?q=mvn+in:readme+" + filter + "&per_page=100&sort=stars&order=desc&page=" + str(page + 1))
+        #r = session.get("https://api.github.com/search/repositories?q=mvn+in:readme+stars:200..30000+&per_page=100&sort=stars&order=desc&page=" + str(page + 1))
+        r = r.json()
+        for item in r['items']:
+            f.write(item["full_name"]+"\n")
+    f.close()
